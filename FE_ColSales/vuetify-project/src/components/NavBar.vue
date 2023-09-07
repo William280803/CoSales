33c5cfad49459622f59a89ac0ab6b38fc7140b93
William <williamalim410@gmail.com>
2023-09-07 22:08:17 +0700

    start/Trello-CoSales-0: Memulai Project Abal-Abal

---
+++ FE_ColSales/vuetify-project/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app-bar app>
+        <v-toolbar-title>CoSales</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text>Home</v-btn>
+        <v-btn text>About</v-btn>
+        <v-btn text>Shop</v-btn>
+        <v-btn text>Contact</v-btn>
+    </v-app-bar>
+</template>
+  
+<script>
+
+</script>
+  
